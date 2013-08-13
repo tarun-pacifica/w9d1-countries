a4c3a01f810c57363454f598dffb5c469cba928e
Pacifica <tarun@pacificasearch.com>
2013-08-13 13:00:26 +1000

    hui

--- countries/app/models/country.rb
+++
@@ -1,5 +0,0 @@
-class Country < ActiveRecord::Base
-  attr_accessible :abbreviation
-  attr_accessible :name
-  attr_accessible :north_america
-end
--- countries/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Integrated::Application.routes.draw do
-
-  root :to => "home#index"
-  get "/countries/:step/:offset" => "home#countries"
-  get "/country/:id" => "home#country"
-
-end
